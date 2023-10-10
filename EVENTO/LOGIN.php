6fa9e07a95886d67f08bac9e28c02aa2d073822b
MIHIR715 <141483751+MIHIR715@users.noreply.github.com>
2023-10-10 23:55:13 +0530

    Add files via upload

---
+++ EVENTO/LOGIN.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LOGIN</title>
+    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="HOME.html">
+</head>
+<body>
+    
+ 
+    <div id="login">
+        
+        <h2>LOGIN</h2>
+        <form action="BACKEND.php">
+
+            <div class="userbox">
+            <input required="true" name="" type="email" class="email" placeholder="Enter your Email">
+            </div>
+            <div class="userbox">
+            <input required="true" name="" type="password" class="password" placeholder="Enter Password">
+            </div>
+            <div>
+            <input type="submit" value="SUBMIT" class="submit" > <input class="reset" type="reset" value="RESET" class="reset">
+            </div>
+            <p>or</p>
+            
+             <p><a href="signin.html" id="c1" target="_parent">create your account</a></p>
+            
+        </form>
+    </div>
+</body>
+</html>
